bc1900f4cd64ce8188ca3d3d79b7ec59dade68de
Eric B <ericjberglund@gmail.com>
2019-07-16 22:39:06 -0500

    Stage 5 Complete

--- Encryption-Decryption (Java)/task/src/encryptdecrypt/Main.java
+++ Encryption-Decryption (Java)/task/src/encryptdecrypt/Main.java
@@ -1,13 +1,23 @@
 package encryptdecrypt;
 import java.util.*;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.io.File;
+import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
 
-        String mode = "";
+        String mode = "enc"; //default value
         String data = "";
+        String encodedDecodedData = "";
         int key = 0;
+        String fileNameIn = "";
+        String fileNameOut = "";
 
+        // collect all the command line arguments
         for(int i = 0; i < args.length; i=i+2) {
             switch (args[i]) {
                 case "-mode":
@@ -19,24 +29,52 @@ public class Main {
                 case "-data":
                     data = args[i+1];
                     break;
+                case "-in":
+                    fileNameIn = args[i+1];
+                    try {
+                        data = readFileAsString(fileNameIn);
+                    } catch(IOException e) {
+                        System.out.println("Cannot read file: " + e.getMessage());
+                    }
+                    break;
+                case "-out":
+                    fileNameOut = args[i+1];
+                    break;
                 default:
-                    mode = "enc";
+                    System.out.println("Unsupported Commands");
+                    break;
             }
         }
 
         if (mode.equals("enc"))
-            encode(data, key);
+            encodedDecodedData = encodeDecode(data, key);
          else if (mode.equals("dec"))
-            encode(data, -key);
+            encodedDecodedData = encodeDecode(data, -key); //negative key un-shifts encrypted data
+
+        // print to STDOUT if no filename is provided
+        if(fileNameOut.equals("")) {
+            System.out.println(encodedDecodedData);
+        // otherwise write data to fileNameOut
+        } else {
+            try(FileWriter writer = new FileWriter(fileNameOut)) {
+                writer.write(encodedDecodedData);
+            } catch (IOException e) {
+                System.out.printf("An exception has occured: %s", e.getMessage());
+            }
+        }
 
     }
 
-    private static void encode(String line, int shiftAmount) {
+    private static String encodeDecode(String line, int shiftAmount) {
         String s = "";
         int len = line.length();
         for (int x = 0; x < len; x++) {
             s += (char) (line.charAt(x) + shiftAmount);
         }
-        System.out.println(s);
+        return s;
+    }
+
+    private static String readFileAsString(String fileName) throws IOException {
+        return new String(Files.readAllBytes(Paths.get(fileName)));
     }
 }
